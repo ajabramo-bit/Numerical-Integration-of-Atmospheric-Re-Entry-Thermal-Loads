b599d212cfc7461b752e5272d9d87d51eb7f7d89
Arianna Abramovich <ajabramo@usc.edu>
2025-12-13 19:08:55 -0800

    updated integrator.py

--- src/integrator.py
+++ src/integrator.py
@@ -2,36 +2,31 @@
 # ASTE404
 # integrator.py
 
+"""
+What this file is responsible for:
+Given an ODE system, how do I advance the solution forward in time?
+"""
+
 import numpy as np
 
 def rk4_step(f, t, y, dt, params):
     """
-    Perform a single 4th-order Runge–Kutta step.
-
-
     Parameters
-    ----------
-    f : function
-    Function returning dy/dt = f(t, y, params)
-    t : float
-    Current time [s]
-    y : ndarray
-    Current state vector
-    dt : float
-    Time step [s]
-    params : dict
-    Dictionary of physical parameters
-
+        f : function - returning dy/dt = f(t, y, params)
+        t : current time [s]
+        y : current state vector
+        dt : time step [s]
+        params : dictionary of physical parameters
 
     Returns
-    -------
-    ndarray
-    Updated state vector after dt
+        ndarray - updated state vector after dt
     """
 
-k1 = f(t, y, params)
-k2 = f(t + dt/2, y + dt/2 * k1, params)
-k3 = f(t + dt/2, y + dt/2 * k2, params)
-k4 = f(t + dt, y + dt * k3, params)
+    k1 = f(t, y, params)                        # slope @ beginning interval
+    k2 = f(t + dt/2, y + dt/2 * k1, params)     # slope @ midpoint
+    k3 = f(t + dt/2, y + dt/2 * k2, params)     # improved midpoint slope
+    k4 = f(t + dt, y + dt * k3, params)         # slope at the end of the interval
 
-return y + (dt / 6.0) * (k1 + 2*k2 + 2*k3 + k4)
+    y_next = y + (dt / 6.0) * (k1 + 2*k2 + 2*k3 + k4)   # weighted average 
+
+    return y_next
